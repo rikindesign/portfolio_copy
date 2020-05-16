23327a4516d32ad64a2556734a39f88ac9835aea
rikindesign <rikinpatel10@yahoo.com>
2020-05-16 12:34:14 -0400

    Updated styles, images and SASS code

--- skew.js
+++ skew.js
@@ -4,6 +4,8 @@ console.log('Skew')
 // Declarations
 const sectionOne = document.getElementById('one')
 const sectionTwo = document.getElementById('two')
+const titleOne = document.getElementById('title-one')
+const titleTwo = document.getElementById('title-two')
 let currentPixel = window.pageYOffset
 
 // Looper Function
@@ -11,12 +13,15 @@ const looper = function() {
     // Checks for no scroll and active animation
     const newPixel = window.pageYOffset
     const diff = newPixel - currentPixel
-    const speedSectionSmall = diff * 0.09
+    const speedSectionSmall = diff * 0.2
     const speedSectionWide = diff * 0.09
 
     // Animated Sections (must include 'will-change: transform;' in SASS)
+    // Set up for-loop for dynamically adding skewing per object-type
     sectionOne.style.transform = 'skewY(' + speedSectionWide + 'deg)';
     sectionTwo.style.transform = 'skewY(' + speedSectionWide + 'deg)';
+    titleOne.style.transform = 'skewY(' + speedSectionSmall + 'deg)';
+    titleTwo.style.transform = 'skewY(' + speedSectionSmall + 'deg)';
 
     currentPixel = newPixel
 
