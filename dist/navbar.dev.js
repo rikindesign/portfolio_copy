79ef18829cdcab73f27b8122efdb25cd963980a6
rikindesign <rikinpatel10@yahoo.com>
2020-10-10 17:19:16 -0400

    Added animations to links

--- dist/navbar.dev.js
+++ dist/navbar.dev.js
@@ -6,8 +6,20 @@ console.log('Navbar');
 var navSlide = function navSlide() {
   var burger = document.querySelector('.burger');
   var nav = document.querySelector('.nav-links');
+  var navLinks = document.querySelectorAll('.nav-links li');
   burger.addEventListener('click', function () {
-    nav.classList.toggle('active-nav');
+    // Navigation Toggling
+    nav.classList.toggle('active-nav'); // Link Animations
+
+    navLinks.forEach(function (link, index) {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = "navLinkFade 0.25s ease forwards ".concat(index / 7 + 0.24, "s");
+      }
+    }); // Burger Animations
+
+    burger.classList.toggle('toggle');
   });
 }; // App Wide Function Calls
 
--- navbar.js
+++ navbar.js
@@ -1,14 +1,30 @@
 // Responsive Navbar JavaScript - R & Design 4.0
-console.log('Navbar');
+console.log('Navbar')
 
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li')
 
     burger.addEventListener('click', () => {
-        nav.classList.toggle('active-nav');
-    });
+
+        // Navigation Toggling
+        nav.classList.toggle('active-nav')
+
+        // Link Animations
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.25s ease forwards ${index / 7 + 0.24}s`
+            }
+        })
+
+        // Burger Animations
+        burger.classList.toggle('toggle')
+    })
+
 }
 
 // App Wide Function Calls
-navSlide();
+navSlide()
