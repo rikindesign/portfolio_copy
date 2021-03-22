313dad4f3ccc003f4b895dbdaf202f1e559972c3
rikindesign <rikinpatel10@yahoo.com>
2021-03-21 22:22:30 -0400

    Completed ChowTime AR case study

--- dist/skew.dev.js
+++ dist/skew.dev.js
@@ -18,7 +18,8 @@ var looper = function looper() {
   var newPixel = window.pageYOffset;
   var diff = newPixel - currentPixel;
   var speedSectionSmall = diff * 0.2;
-  var speedSectionWide = diff * 0.05; // Animated Sections (must include 'will-change: transform;' in SASS)
+  var speedSectionWide = diff * 0.05; //diff * 0.05
+  // Animated Sections (must include 'will-change: transform;' in SASS)
   // Set up for-loop for dynamically adding skewing per object-type
 
   sectionOne.style.transform = 'skewY(' + speedSectionWide + 'deg)';
